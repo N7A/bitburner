a3f7748bb861b6f4bac2044cf539e5d6b498bc85
Arthur <arthurnicolas2@hotmail.fr>
2025-08-02 10:04:41 +0200

    refactor: amelioration log share ram

--- workspace/faction/share-ram.looper.ts
+++ workspace/faction/share-ram.looper.ts
@@ -4,7 +4,10 @@ import * as ExecutionsRepository from 'workspace/domain/executions.repository'
 /**
  * Share RAM to faction.
  */
-export async function main(ns: NS) {    
+export async function main(ns: NS) {
+    // load input arguments
+	const input: InputArg = getInput(ns);
+ 
     setupDashboard(ns);
 
     ns.atExit(() => {
@@ -12,34 +15,52 @@ export async function main(ns: NS) {
     })
     const startTime = new Date();
 
-    while (true) {
+    do {
         const shareStartTime = new Date();
         await ns.share();
         const shareEndTime =  new Date();
         refreshDashBoard(ns, startTime, shareStartTime, shareEndTime);
-    }
+    } while (input.runHasLoop)
 }
 
-/** @param {import(".").NS } ns */
+//#region Input arguments
+type InputArg = {
+	/** Serveur cible */
+	runHasLoop: boolean;
+}
+
+/**
+ * Load input arguments
+ * @param ns Bitburner API
+ * @returns 
+ */
+function getInput(ns: NS): InputArg {
+	return {
+		runHasLoop: ns.args[0] ? (ns.args[0] as boolean) : true
+	};
+}
+//#endregion Input arguments
+
+//#region Dashboard
 function setupDashboard(ns: NS) {
 	ns.disableLog("ALL");
     ns.enableLog("share")
     ns.clearLog();
 	
-	ns.ui.openTail();
-	ns.ui.setTailTitle(`[${ns.getHostname()}] Share RAM`);
+    Log.initTailTitle(ns, 'Share RAM', 'looper', ns.getHostname());
 	ns.ui.moveTail(1200, 50);
 	
 	ns.print('Waiting to share...');
+	ns.ui.openTail();
 }
 
-/** @param {import(".").NS } ns */
 function refreshDashBoard(ns: NS, startTime: Date, shareStartTime: Date, shareEndTime: Date) {
     ns.clearLog();
     const shareDuration = new Date(shareEndTime.getTime() - shareStartTime.getTime())
     ns.print(Log.getStartLog());
-    ns.print(Log.color("Thread start time : ", Log.Color.MAGENTA), startTime.toLocaleTimeString());
-    ns.print(Log.color("Last share time : ", Log.Color.MAGENTA),  shareEndTime.toLocaleTimeString());
-    ns.print(Log.color("Last share duration : ", Log.Color.MAGENTA),  shareDuration.toLocaleTimeString());
+    ns.print(Log.color("Thread start time : ", Log.Color.MAGENTA), Log.date(ns, startTime));
+    ns.print(Log.color("Last share time : ", Log.Color.MAGENTA),  Log.date(ns, shareEndTime));
+    ns.print(Log.color("Last share duration : ", Log.Color.MAGENTA),  Log.date(ns, shareDuration));
     ns.print(Log.getEndLog());
 }
+//#endregion Dashboard
