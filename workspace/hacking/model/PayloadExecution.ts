797a5240c31bd5f10a16a1a1ec7d5c3735e15adc
PC-FIXE-N74-2023 <arthurnicolas2@hotmail.fr>
2025-08-07 20:21:36 +0200

    refactor: clean payload launcher

--- workspace/hacking/model/PayloadExecution.ts
+++ workspace/hacking/model/PayloadExecution.ts
@@ -11,19 +11,21 @@ export const GROW_SCRIPT = Referentiel.HACKING_DIRECTORY + '/payload/grow.looper
 
 export class PayloadExecution implements RamResourceExecution {
     private scripts: string[];
+    private targetHost: string;
 
     constructor(ns: NS, targetHost: string) {
         const scripts: string[] = [];
+        this.targetHost = targetHost
     
         // load host data
-        const data: ServerData|null = ServersRepository.get(ns, targetHost);
+        const data: ServerData|null = ServersRepository.get(ns, this.targetHost);
         const hackData: HackData = data!.hackData;
     
         scripts.push(HACK_SCRIPT);
         scripts.push(WEAKEN_SCRIPT);
     
         if (hackData.moneyMax === 0) {
-            ns.tprint('WARN', ' ', '[', targetHost, '] ', 'No money in there');
+            ns.print('WARN', ' ', '[', this.targetHost, '] ', 'No money in there');
         } else {
             scripts.push(GROW_SCRIPT);
             scripts.push(WEAKEN_SCRIPT);
@@ -33,6 +35,13 @@ export class PayloadExecution implements RamResourceExecution {
     }
 
     getScript() {
+        // TODO : check if payload already running
+        //ns.getRunningScript(Properties.HACKING_DIRECTORY + '/payload/hack.looper.ts', targetHost)
+        //ns.getRunningScript(Properties.HACKING_DIRECTORY + '/payload/hack.looper.ts', 'home', targetHost)
+        
+        // TODO : split nb thread (plutot que nb RAM) <- pour que le grow et weaken aient la force pour soutenir le hack
+        // TODO : dynamique répartition : hack then weaken then grow then weaken then repeate
+        // TODO : weaken thread nb >= grow + hack thread nb; grow thread nb >= hack thread nb
         return this.scripts;
     }
 
--- workspace/hacking/payload/payload.launcher.ts
+++ workspace/hacking/payload/payload.launcher.ts
@@ -1,19 +1,7 @@
-import * as Referentiel from 'workspace/referentiel'
-import {ServerData, HackData} from 'workspace/domain/servers/model/ServerData'
 import * as Log from 'workspace/frameworks/logging';
-import * as ServersRepository from 'workspace/domain/servers/servers.repository';
 import * as TargetsRepository from 'workspace/domain/targets/targets.repository';
 import * as ExecutionsRepository from 'workspace/domain/executions/executions.repository';
-import { getAvailableServer } from 'workspace/load-balancer/main'
-import { execFitRam } from 'workspace/load-balancer/fit-ram.service'
-import {ExecutionParameters, ScriptParameters} from 'workspace/load-balancer/model/ExecutionServer'
 import { OrderType } from 'workspace/domain/executions/model/Order';
-
-//#region Constants
-export const HACK_SCRIPT = Referentiel.HACKING_DIRECTORY + '/payload/hack.looper.ts';
-export const WEAKEN_SCRIPT = Referentiel.HACKING_DIRECTORY + '/payload/weaken.looper.ts';
-export const GROW_SCRIPT = Referentiel.HACKING_DIRECTORY + '/payload/grow.looper.ts';
-//#endregion Constants
 
 /**
  * Hack les cibles données par le unlock.
@@ -28,94 +16,9 @@ export async function main(ns: NS) {
         // remove from hack targets
         TargetsRepository.removeHack(ns, target);
 
-        //#region Payload
-
-        // free self RAM script and trigger payload
-        const scripts = getPayloadScript(ns, target);
-
         ExecutionsRepository.add(ns, {type: OrderType.HACK, target: target, weight: 1});
-
-        // TODO : check if payload already running
-        //ns.getRunningScript(Properties.HACKING_DIRECTORY + '/payload/hack.looper.ts', targetHost)
-        //ns.getRunningScript(Properties.HACKING_DIRECTORY + '/payload/hack.looper.ts', 'home', targetHost)
-        
-        // TODO : split nb thread (plutot que nb RAM) <- pour que le grow et weaken aient la force pour soutenir le hack
-        // TODO : dynamique répartition : hack then weaken then grow then weaken then repeate
-        // TODO : weaken thread nb >= grow + hack thread nb; grow thread nb >= hack thread nb
-        const payloadRam = scripts.map(x => ns.getScriptRam(x, target)).reduce((x, y) => x + y);
-
-        // check RAM availability to priorize
-        const execution: ExecutionParameters = getAvailableServer(ns, payloadRam, 1, target);
-
-        let payload: Payload = new HomePayload(ns);
-        if (execution.hostname === target && execution.nbThread > 0) {
-            payload = new SelfPayload(ns);
-        }
-
-        //await payload.execute(execution.hostname, target);
-        //#endregion Payload
 
         ns.print(`END ${Log.action('Payload')} ${Log.target(target)}`);
         ns.print(Log.getEndLog());
-    }
-}
-
-function getPayloadScript(ns:NS, targetHost: string): string[] {
-    const scripts: string[] = [];
-
-    // load host data
-    const data: ServerData|null = ServersRepository.get(ns, targetHost);
-    const hackData: HackData = data!.hackData;
-
-    scripts.push(HACK_SCRIPT);
-    scripts.push(WEAKEN_SCRIPT);
-
-    if (hackData.moneyMax === 0) {
-        ns.tprint('WARN', ' ', '[', targetHost, '] ', 'No money in there');
-    } else {
-        scripts.push(GROW_SCRIPT);
-        scripts.push(WEAKEN_SCRIPT);
-    }
-    return scripts;
-}
-interface Payload {
-    execute(sourceHost: string, targetHost: string): Promise<void>;
-}
-
-class HomePayload implements Payload {
-    private ns:NS;
-
-    constructor(ns:NS) {
-        this.ns = ns;
-    }
-    
-    async execute(sourceHost: string, targetHost: string): Promise<void> {
-        this.ns.tprint('WARN', ' ', `Payload on self (${targetHost}) impossible`);
-        const scripts = getPayloadScript(this.ns, targetHost);
-        for (const script of scripts) {
-            let pidExec = this.ns.exec(script, sourceHost, 1, targetHost)
-
-            this.ns.tprint(`${targetHost} : ${script} ${pidExec == 0 ? 'KO': 'OK'}`);
-        }
-    }
-}
-
-/**
- * Execute payload on distant
- */
-class SelfPayload implements Payload {
-    private ns:NS;
-
-    constructor(ns:NS) {
-        this.ns = ns;
-    }
-
-    async execute(sourceHost: string, targetHost: string): Promise<void> {
-        this.ns.tprint('SUCCESS', ' ', `Payload on self (${targetHost}) possible`);
-        const scripts = getPayloadScript(this.ns, targetHost);
-        // execute payload on distant
-        await execFitRam(this.ns, [targetHost], scripts.map(x => {
-            return {scriptsFilepath: x} as ScriptParameters
-        }));
     }
 }
