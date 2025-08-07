f04d84dc064c6751dab6a6e7532c50b3f8fca737
Arthur <arthurnicolas2@hotmail.fr>
2025-08-07 22:24:19 +0200

    feat: clean old fit ram

--- workspace/hacking/payload/fit-payload.launcher.ts
+++ workspace/hacking/payload/fit-payload.launcher.ts
@@ -1,35 +1,15 @@
-import * as Referentiel from 'workspace/referentiel'
-import {execFitRam} from 'workspace/load-balancer/fit-ram.service'
-import {ScriptParameters} from 'workspace/load-balancer/model/ExecutionServer'
-import * as OwnedServersRepository from 'workspace/domain/owned-servers.repository'
 import * as ServersRepository from 'workspace/domain/servers/servers.repository'
 import {main as bestHackFitTarget} from 'workspace/hacking/payload/payload.selector'
+import * as ExecutionsRepository from 'workspace/domain/executions/executions.repository';
+import { OrderType } from 'workspace/domain/executions/model/Order';
 
 export async function main(ns: NS) {
-    const scripts = [
-        Referentiel.HACKING_DIRECTORY + '/payload/grow.looper.ts',
-        Referentiel.HACKING_DIRECTORY + '/payload/weaken.looper.ts',
-        Referentiel.HACKING_DIRECTORY + '/payload/hack.looper.ts',
-        Referentiel.HACKING_DIRECTORY + '/payload/weaken.looper.ts'
-    ];
-
-    const targets: string[] = ServersRepository.getAll(ns);
+    const targets: string[] = ServersRepository.getAll(ns)
+        .map(x => ServersRepository.get(ns, x))
+        .filter(x => x.state.unlocked)
+        .map(x => x.name);
 
     const targetHost: string | undefined = await bestHackFitTarget(ns, targets);
 
-    const availableServers: string[] = OwnedServersRepository.getAll(ns).map(x => x.hostname);
-
-    if (targetHost === undefined) {
-        ns.exit();
-    }
-
-    ns.tprint(`Fit target : ${targetHost}`);
-
-	execFitRam(
-		ns, 
-		availableServers,
-		scripts.map(x => {
-            return {scriptsFilepath: x, args: [targetHost]} as ScriptParameters
-        })
-	);
+    ExecutionsRepository.add(ns, {type: OrderType.HACK, target: targetHost, weight: 1});
 }
