99774c29b5bdee3d0c3d6820faf59ef2798c980a
Arthur <arthurnicolas2@hotmail.fr>
2025-08-23 14:48:24 +0200

    feat: orga buy stock

--- src/workspace/resource-generator/stock-market/buy-stock-train.scheduler.ts
+++ src/workspace/resource-generator/stock-market/buy-stock-train.scheduler.ts
@@ -1,49 +1,83 @@
 import { getMaxShares, selectBestTrainEnMarche, waitRepayTime } from "workspace/resource-generator/stock-market/buy-stock.selector";
 import { MoneyPiggyBankService } from 'workspace/piggy-bank/money-piggy-bank.service'
 import * as Log from 'workspace/frameworks/logging';
+import { Logger } from 'workspace/common/Logger';
 
 export async function main(ns: NS) {
-    ns.disableLog('sleep');
-    ns.clearLog();
-    ns.ui.openTail();
+    const main: Main = new Main(ns);
 
-    const workStartTime = new Date();
-    ns.print(`${Log.time(new Date(Date.now()))} - Start`)
+    await main.run();
+}
 
-    const moneyPiggyBankService = new MoneyPiggyBankService(ns);
+class Main {
+    private ns: NS;
+    private logger: Logger;
+    private stockSymbol: string;
 
-    let shares: number = 0;
-    let stockSymbol: string | undefined;
-    do {
-        stockSymbol = selectBestTrainEnMarche(ns);
+    constructor(ns: NS) {
+        this.ns = ns;
+        this.logger = new Logger(ns);
+        this.setupDashboard();
+    }
 
-        if (!stockSymbol) {
-            return;
-        }
-        ns.print(`Best to buy : ${stockSymbol}`);
+    async run() {
+        this.logger.log(`${Log.time(new Date(Date.now()))} - Start`);
+        const workStartTime = new Date();
+        await this.buy();
+        const profit = await this.sell();
+        const workEndTime = new Date();
+        const workDuration = new Date(workEndTime.getTime() - workStartTime.getTime());
+        this.logger.log(Log.INFO("Time to profit",  Log.time(workDuration)));
+        this.logger.log(Log.INFO("Production", `\$${this.ns.formatNumber(profit / workDuration.getTime()*1000)} / s`));
+        this.logger.log(`${Log.time(new Date(Date.now()))} - End`);
+    }
 
-        const availableMoney = Math.min(1000*1000*1000, moneyPiggyBankService.getDisponibleMoney(ns.getPlayer().money));
-        shares = getMaxShares(ns, stockSymbol, availableMoney);
-    } while(shares === 0)
+    async buy(): Promise<string> {
+        const moneyPiggyBankService = new MoneyPiggyBankService(this.ns);
+
+        let shares: number = 0;
+        do {
+            this.stockSymbol = selectBestTrainEnMarche(this.ns);
+            if (!this.stockSymbol) {
+                return;
+            }
+            this.logger.log(`Best to buy : ${this.stockSymbol}`);
+
+            const availableMoney = Math.min(1000*1000*1000, moneyPiggyBankService.getDisponibleMoney(this.ns.getPlayer().money));
+            shares = getMaxShares(this.ns, this.stockSymbol, availableMoney);
+        } while(shares === 0)
+        
+        const buyPrice = this.ns.stock.buyStock(this.stockSymbol, shares);
+        const spent = buyPrice * shares;
+        this.logger.log(`Buy ${this.ns.formatNumber(shares)} ${this.stockSymbol} for \$${this.ns.formatNumber(spent)} (\$${this.ns.formatNumber(buyPrice)})`);
+
+        return this.stockSymbol;
+    }
+
+    async sell(): Promise<number> {
+        const sharesLong = this.ns.stock.getPosition(this.stockSymbol)[0];
+        const buyPriceLong = this.ns.stock.getPosition(this.stockSymbol)[1]
+        const spent = buyPriceLong*sharesLong + 2*this.ns.stock.getConstants().StockMarketCommission;
+        const askPriceWaiting = spent / sharesLong;
+        // TODO : split script buy / sell -> multi buy possible avant sell
+        this.logger.log(`Wait repay time (Ask Price: \$${this.ns.formatNumber(askPriceWaiting)})...`);
+        await waitRepayTime(this.ns, this.stockSymbol, buyPriceLong*sharesLong, sharesLong);
+
+        const sellPrice = this.ns.stock.sellStock(this.stockSymbol, sharesLong);
+        const gain = sellPrice * sharesLong;
+        this.logger.log(`Sell ${this.ns.formatNumber(sharesLong)} ${this.stockSymbol} for \$${this.ns.formatNumber(sellPrice)}`);
+        this.logger.log(`Profit : \$${this.ns.formatNumber(gain - spent)}`);
+        return gain - spent
+    }
     
-    const buyPrice = ns.stock.buyStock(stockSymbol, shares);
-    const spent = buyPrice * shares;
-    ns.print(`${Log.time(new Date(Date.now()))} - Buy ${ns.formatNumber(shares)} ${stockSymbol} for \$${ns.formatNumber(spent)} (\$${ns.formatNumber(buyPrice)})`);
-
-    const sharesLong = ns.stock.getPosition(stockSymbol)[0];
-    const buyPriceLong = ns.stock.getPosition(stockSymbol)[1]
-    const askPriceWaiting = (buyPriceLong*sharesLong + 2*ns.stock.getConstants().StockMarketCommission) / sharesLong;
-    // TODO : split script buy / sell -> multi buy possible avant sell
-    ns.print(`${Log.time(new Date(Date.now()))} - Wait repay time (Ask Price: \$${ns.formatNumber(askPriceWaiting)})...`);
-    await waitRepayTime(ns, stockSymbol, buyPriceLong*sharesLong, sharesLong);
-
-    const sellPrice = ns.stock.sellStock(stockSymbol, sharesLong);
-    const gain = sellPrice * sharesLong;
-    ns.print(`${Log.time(new Date(Date.now()))} - Sell ${ns.formatNumber(sharesLong)} ${stockSymbol} for \$${ns.formatNumber(sellPrice)}`);
-    ns.print(`Profit : \$${ns.formatNumber(gain - spent)}`);
-    const workEndTime = new Date();
-    const workDuration = new Date(workEndTime.getTime() - workStartTime.getTime());
-    ns.print(Log.INFO("Time to profit",  Log.time(workDuration)));
-    ns.print(Log.INFO("Production", `\$${ns.formatNumber((gain - spent) / workDuration.getTime()*1000)} / s`));
-    ns.print(`${Log.time(new Date(Date.now()))} - End`);
+    //#region Dashboard
+    private setupDashboard() {
+        this.ns.disableLog('sleep');
+        this.ns.clearLog();
+        
+        Log.initTailTitle(this.ns, 'Buy max forecast stock', 'scheduler');
+        
+        this.ns.ui.openTail();
+    }
+    //#endregion Dashboard
 }
