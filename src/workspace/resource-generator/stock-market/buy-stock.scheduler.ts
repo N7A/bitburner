add329eb09132ac1aebc17eeb3c8cece7872035c
Arthur <arthurnicolas2@hotmail.fr>
2025-08-23 21:49:35 +0200

    feat: add hunterdaemon to buy stock

--- src/workspace/resource-generator/stock-market/buy-stock.scheduler.ts
+++ src/workspace/resource-generator/stock-market/buy-stock.scheduler.ts
@@ -1,3 +1,4 @@
+import { Headhunter } from 'workspace/common/headhunter';
 import { getMaxShares, selectBestBuyQuick, waitBuyTime } from "workspace/resource-generator/stock-market/buy-stock.selector";
 import { MoneyPiggyBankService } from 'workspace/piggy-bank/money-piggy-bank.service'
 import * as Log from 'workspace/frameworks/logging';
@@ -5,10 +6,37 @@ import { Logger } from 'workspace/common/Logger';
 import { SellStockWorker } from "workspace/resource-generator/stock-market/sell-stock.worker";
 
 export async function main(ns: NS) {
+    // load input arguments
+	const input: InputArg = getInput(ns);
     const main: Main = new Main(ns);
 
-    await main.run();
+    // waitNewTargets = true : contracts appear over the time
+    const daemon = new Headhunter<string>(ns, () => main.getTargets(), main.run, true);
+    
+    if (!input.runHasLoop) {
+        daemon.killAfterLoop();
+    }
+    
+    daemon.run();
 }
+
+//#region Input arguments
+type InputArg = {
+	/** Serveur cible */
+	runHasLoop: boolean;
+}
+
+/**
+ * Load input arguments
+ * @param ns Bitburner API
+ * @returns 
+ */
+function getInput(ns: NS): InputArg {
+	return {
+		runHasLoop: ns.args[0] ? (ns.args[0] as boolean) : false
+	};
+}
+//#endregion Input arguments
 
 class Main {
     private ns: NS;
@@ -24,6 +52,8 @@ class Main {
     async run() {
         this.logger.log(`Start`);
         const workStartTime = new Date();
+        await this.getTargets();
+        this.logger.log(`Best to buy : ${Log.target(this.stockSymbol)}`);
         // TODO : multi buy possible avant sell
         await this.buy();
         const sellWorker: SellStockWorker = new SellStockWorker(this.ns, {stockSymbol: this.stockSymbol});
@@ -35,7 +65,7 @@ class Main {
         this.logger.log(`End`);
     }
 
-    async buy(): Promise<string> {
+    async getTargets(): Promise<string[]> {
         const moneyPiggyBankService = new MoneyPiggyBankService(this.ns);
 
         let shares: number = 0;
@@ -46,7 +76,6 @@ class Main {
                 await this.ns.asleep(500);
                 continue;
             }
-            this.logger.log(`Best to buy : ${Log.target(this.stockSymbol)}`);
 
             this.logger.waiting('buy time');
             await waitBuyTime(this.ns, this.stockSymbol);
@@ -55,6 +84,15 @@ class Main {
             const availableMoney = Math.min(1000*1000*1000, moneyPiggyBankService.getDisponibleMoney(this.ns.getPlayer().money));
             shares = getMaxShares(this.ns, this.stockSymbol, availableMoney);
         } while(shares === 0)
+
+        return [this.stockSymbol]
+    }
+    
+    async buy(): Promise<string> {
+        const moneyPiggyBankService = new MoneyPiggyBankService(this.ns);
+
+        const availableMoney = Math.min(1000*1000*1000, moneyPiggyBankService.getDisponibleMoney(this.ns.getPlayer().money));
+        const shares: number = getMaxShares(this.ns, this.stockSymbol, availableMoney);
         
         const buyPrice = this.ns.stock.buyStock(this.stockSymbol, shares);
         const spent = buyPrice * shares;
