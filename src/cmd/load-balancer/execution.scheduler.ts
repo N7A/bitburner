ec79cfa8ff7737deacfe80aa4e23d002365a6311
Arthur <arthurnicolas2@hotmail.fr>
2025-08-27 00:22:13 +0200

    fix(load-balancer): get arg input

--- src/cmd/load-balancer/execution.scheduler.ts
+++ src/cmd/load-balancer/execution.scheduler.ts
@@ -92,7 +92,7 @@ type InputArg = {
  */
 function getInput(ns: NS): InputArg {
     return {
-        runHasLoop: ns.args !== undefined ? (ns.args[0] as boolean) : true
+        runHasLoop: ns.args[0] !== undefined ? (ns.args[0] as boolean) : true
     };
 }
 //#endregion Input arguments
