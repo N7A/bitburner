fdaa9dc8482c848a5a1c0e8f63e24f7af83f22c4
Arthur <arthurnicolas2@hotmail.fr>
2025-08-29 23:53:27 +0200

    feat(load-balancer): migration daemon

--- src/cmd/load-balancer/upgrade-servers.scheduler.ts
+++ src/cmd/load-balancer/upgrade-servers.scheduler.ts
@@ -3,51 +3,21 @@ import {selectUpgrade} from 'workspace/load-balancer/upgrade-servers.selector'
 import {executeUpgrade} from 'workspace/load-balancer/upgrade-servers.worker'
 import { UpgradeExecution } from 'workspace/load-balancer/model/UpgradeExecution'
 import { MoneyPiggyBankService } from 'workspace/piggy-bank/money-piggy-bank.service';
+import { Daemon } from 'workspace/socle/interface/daemon';
 
 export async function main(ns: NS) {
     // load input arguments
 	const input: InputArg = getInput(ns);
 
-    setupDashboard(ns);
+    const daemon = new UpgradeServersDaemon(ns);
     
-    //#region Utils
-	const getMoney = () => ns.getPlayer().money;
-    //#endregion
+    daemon.setupDashboard();
+
+    if (!input.runHasLoop) {
+        daemon.killAfterLoop();
+    }
     
-    const moneyPiggyBankService = new MoneyPiggyBankService(ns);
-
-    do {
-        ns.print(Log.getStartLog());
-        // select next upgrade
-        let nextUpgrade: UpgradeExecution | undefined = await selectUpgrade(ns);
-        if (!nextUpgrade) {
-            continue;
-        }
-
-        ns.print(Log.INFO('Prochain coût', `${Log.money(ns, nextUpgrade.cost)} (${ns.formatRam(nextUpgrade.ram)})`));
-
-        ns.print('Waiting to have enough money...');
-        // wait purchase to be possible
-        while(moneyPiggyBankService.getDisponibleMoney(getMoney()) < nextUpgrade.cost) {
-            // sleep to prevent crash because of infinite loop
-            await ns.sleep(500);
-        }
-
-        // get best purchase with max amount disponible
-        const selectedUpgrade = await selectUpgrade(ns, moneyPiggyBankService.getDisponibleMoney(getMoney()));
-        
-        ns.print('Buy upgrade');
-        // do purchase
-        await executeUpgrade(ns, selectedUpgrade);
-        
-        ns.print(Log.getEndLog());
-        if (input.runHasLoop) {
-            // sleep to prevent crash because of infinite loop
-            await ns.sleep(1000 * 60 * 1);
-        }
-	} while (input.runHasLoop)
-    
-    ns.ui.closeTail();
+    await daemon.run();
 }
 
 //#region Input arguments
@@ -68,14 +38,65 @@ function getInput(ns: NS): InputArg {
 }
 //#endregion Input arguments
 
-//#region Dashboard
-function setupDashboard(ns: NS) {
-    ns.disableLog("ALL");
-    ns.clearLog();
+class UpgradeServersDaemon extends Daemon {
+    private moneyPiggyBankService: MoneyPiggyBankService;
+
+    constructor(ns: NS) {
+        super(ns);
+
+        this.moneyPiggyBankService = new MoneyPiggyBankService(ns);
+    }
+
+    async work(): Promise<any> {
+        // select next upgrade
+        let nextUpgrade: UpgradeExecution | undefined = await this.getNextExecution();
+        if (!nextUpgrade) {
+            return;
+        }
+        this.ns.print(Log.INFO('Prochain coût', `${Log.money(this.ns, nextUpgrade.cost)} (${this.ns.formatRam(nextUpgrade.ram)})`));
+
+        // Waiting to have enough money
+        this.ns.print('Waiting to have enough money...');
+        await this.waitExecutionTime(nextUpgrade);
+
+        // do purchase
+        await this.execute();
+    }
+
+    async getNextExecution() {
+        return await selectUpgrade(this.ns);
+    }
+
+    async waitExecutionTime(nextUpgrade: UpgradeExecution) {
+        // wait purchase to be possible
+        while(this.moneyPiggyBankService.getDisponibleMoney(this.getMoney()) < nextUpgrade.cost) {
+            // sleep to prevent crash because of infinite loop
+            await this.ns.sleep(500);
+        }
+    }
+
+    async execute() {
+        // get best purchase with max amount disponible
+        const selectedUpgrade = await selectUpgrade(this.ns, this.moneyPiggyBankService.getDisponibleMoney(this.getMoney()));
+        
+        this.ns.print('Buy upgrade');
+        // do purchase
+        await executeUpgrade(this.ns, selectedUpgrade);
+    }
+
+    //#region Utils
+	getMoney = () => this.ns.getPlayer().money;
+    //#endregion Utils
     
-    Log.initTailTitle(ns, 'Upgrade server', 'scheduler');
-    
-    ns.print('Starting...');
-    ns.ui.openTail();
+    //#region Dashboard
+    setupDashboard() {
+        this.ns.disableLog("ALL");
+        this.ns.clearLog();
+        
+        Log.initTailTitle(this.ns, 'Upgrade server', 'scheduler');
+        
+        this.ns.print('Starting...');
+        this.ns.ui.openTail();
+    }
+    //#endregion Dashboard
 }
-//#endregion Dashboard
