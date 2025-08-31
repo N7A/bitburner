0e70457dec34bb059de2bc4d061cea6fa7370cbd
Arthur <arthurnicolas2@hotmail.fr>
2025-08-31 03:53:54 +0200

    refactor: migration execution scheduler to daemon

--- src/cmd/load-balancer/execution.scheduler.ts
+++ src/cmd/load-balancer/execution.scheduler.ts
@@ -9,67 +9,25 @@ import { ExecutionsRepository } from 'workspace/load-balancer/domain/executions.
 import { waitEndExecution } from 'workspace/socle/utils/execution';
 import { getRepartitions } from 'workspace/load-balancer/execution-server.selector';
 import { ExecutionOrdersService } from 'workspace/load-balancer/execution-orders.service';
+import { Daemon } from 'workspace/socle/interface/daemon';
 
 export async function main(ns: NS) {
     // load input arguments
     const input: InputArg = getInput(ns);
 
-    setupDashboard(ns);
-
-    const executionsRepository = new ExecutionsRepository(ns);
-    const executionOrdersService = new ExecutionOrdersService(ns);
-
-    let orders: RamResourceExecution[] = [];
-
+    const daemon: ExecutionSchedulerDaemon = new ExecutionSchedulerDaemon(ns);
+    
     ns.atExit(() => {
-        orders.map(x => x.request).forEach(x => resetRunningProcess(ns, x));
+        daemon.resetAllRunningProcess();
     });
     
-    do {
-        //ns.clearLog();
-        ns.print(Log.getStartLog());
+    daemon.setupDashboard();
 
-        ns.print('Waiting request modification...');
-        // wait until orders change
-        await waitContextChange(ns);
-
-        // TODO : dont kill hack and setup
-        // kill all old for recalcul repartition
-        orders.map(x => x.request).forEach(x => resetRunningProcess(ns, x));
-        // maj orders
-        orders = executionOrdersService.getExecutionOrders();
-
-        ns.print(Log.action('Define servers repartion'));
-        // define servers repartion
-        const executions: Map<RamResourceExecution, ExecutionOrder[]> = await getRepartitions(ns, orders);
-
-        ns.print(Log.action('Executions'), ` (${executions.size})`);
-        // lancement des scripts
-        for (const process of executions.keys()) {
-            let currentExecutionOrders = executions.get(process)
-            if (!currentExecutionOrders) {
-                continue;
-            }
-            for(const executionOrder of currentExecutionOrders) {
-                const pids = await execute(ns, executionOrder);
-                
-                // TODO : setup dashboard, pour reduire au minimum la ram
-                pids.filter(x => x !==0)
-                    .forEach(x => {
-                        process.setupDashboard(ns, x, executionOrder.sourceHostname);
-                    })
-                ns.print(`${process.getActionLog()} ${ns.formatNumber(executionOrder.nbThread, 0)} threads on ${Log.source(executionOrder.sourceHostname)}`);
-                // maj pid processes
-                process.request.pid = [...(process.request.pid ?? []), ...pids];
-            }
-            executionsRepository.save(process.request)
-        }
-
-        ns.print(Log.getEndLog());
-        if (input.runHasLoop) {
-            await ns.asleep(500);
-        }
-    } while (input.runHasLoop)
+    if (!input.runHasLoop) {
+        daemon.killAfterLoop();
+    }
+    
+    await daemon.run();
     
     ns.ui.closeTail();
 }
@@ -92,104 +50,157 @@ function getInput(ns: NS): InputArg {
 }
 //#endregion Input arguments
 
-//#region Dashboard
-function setupDashboard(ns: NS) {
-    ns.disableLog("ALL");
-    ns.clearLog();
-    
-    Log.initTailTitle(ns, 'Executions', 'Scheduler');
-    ns.ui.openTail();
-}
-//#endregion Dashboard
+class ExecutionSchedulerDaemon extends Daemon {
+    private orders: RamResourceExecution[] = [];
+    private executionsRepository: ExecutionsRepository;
+    private executionOrdersService: ExecutionOrdersService;
+    private serversService: ServersService;
 
-/**
- * Kill all process linked with process request, then clear pid on repository.
- * @param ns 
- * @param request process request to reset
- */
-function resetRunningProcess(ns: NS, request: ProcessRequest) {
-    const executionsRepository = new ExecutionsRepository(ns);
+    constructor(ns: NS) {
+        super(ns);
 
-    // kill all process
-    request.pid?.filter(x => x !== undefined).forEach(x => ns.kill(x));
-    // reset all repository pid
-    request.pid = [];
-    executionsRepository.save(request)
-}
-
-/**
- * Wait process orders or available ram change.
- * @param ns 
- * @returns new process orders
- */
-async function waitContextChange(ns: NS): Promise<void> {
-    const executionOrdersService = new ExecutionOrdersService(ns);
-    const serversService = new ServersService(ns);
-
-    const requests: RamResourceExecution[] = executionOrdersService.getExecutionOrders();
-    const ramDisponible = serversService.getAllExecutable()
-            .map(x => ns.getServerMaxRam(x))
-            //.map(x => availableRam(ns, x))
-            .reduce((a,b) => a+b);
-
-    const getId = (request: RamResourceExecution) => request.request.type + (request.request.target ?? '');
-
-    let newRequest: RamResourceExecution[];
-    let newRamDisponible: number;
-    
-    do {
-        await ns.sleep(500);
-
-        newRamDisponible = serversService.getAllExecutable()
-            .map(x => ns.getServerMaxRam(x))
-            //.map(x => availableRam(ns, x))
-            .reduce((a,b) => a+b);
-
-        // remove from orders manual killed or script KO 
-        // TODO : alert ?
-        /*const killedOrders: RamResourceExecution[] = requests.filter(x => x.request.pid?.some(y => !ns.isRunning(y)));
-        if (killedOrders.length > 0) {
-            killedOrders.forEach(x => ExecutionsRepository.remove(ns, x.request));
-            ns.print('Execution order killed : ', killedOrders.map(x => x.request.type + ' ' + x.request.target));
-            break;
-        }*/
-        
-        newRequest = executionOrdersService.getExecutionOrders();
-    } while (
-        // requetes inchangées
-        Array.from(new Set([...requests.map(x => getId(x)), ...newRequest.map(x => getId(x))]))
-            .every(x => newRequest.map(x => getId(x)).includes(x) && requests.map(x => getId(x)).includes(x))
-        // RAM inchangée
-        && newRamDisponible === ramDisponible
-    )
-}
-
-//#region Execution
-async function execute(ns: NS, executionOrder: ExecutionOrder): Promise<number[]> {
-    const logger = new TerminalLogger(ns);
-    let pids: number[] = []
-    if (executionOrder.nbThread === 0) {
-        return pids;
+        this.executionsRepository = new ExecutionsRepository(ns);
+        this.executionOrdersService = new ExecutionOrdersService(ns);
+        this.serversService = new ServersService(ns);
     }
 
-    for (const script of executionOrder.request.scripts) {
-        // setup
-        if (!ns.fileExists(script.scriptsFilepath, executionOrder.sourceHostname)) {
-            logger.warn(`Script ${script.scriptsFilepath} inexistant sur ${executionOrder.sourceHostname}`);
-            const copyPid = ns.run(Referentiel.HACKING_DIRECTORY + '/spreading/copy-toolkit.worker.ts', 1, executionOrder.sourceHostname);
-            
-            await waitEndExecution(ns, copyPid);
+    async work(): Promise<any> {
+        //ns.clearLog();
+        this.ns.print('Waiting request modification...');
+        // wait until orders change
+        await this.waitContextChange();
+
+        // TODO : dont kill hack and setup
+        // kill all old for recalcul repartition
+        this.resetAllRunningProcess();
+        // maj orders
+        this.orders = this.executionOrdersService.getExecutionOrders();
+
+        this.ns.print(Log.action('Define servers repartion'));
+        // define servers repartion
+        const executions: Map<RamResourceExecution, ExecutionOrder[]> = await getRepartitions(this.ns, this.orders);
+
+        this.ns.print(Log.action('Executions'), ` (${executions.size})`);
+        // lancement des scripts
+        for (const process of executions.keys()) {
+            let currentExecutionOrders = executions.get(process)
+            if (!currentExecutionOrders) {
+                continue;
+            }
+            for(const executionOrder of currentExecutionOrders) {
+                const pids = await this.execute(executionOrder);
+                
+                // TODO : setup dashboard, pour reduire au minimum la ram
+                pids.filter(x => x !==0)
+                    .forEach(x => {
+                        process.setupDashboard(this.ns, x, executionOrder.sourceHostname);
+                    })
+                this.ns.print(`${process.getActionLog()} ${this.ns.formatNumber(executionOrder.nbThread, 0)} threads on ${Log.source(executionOrder.sourceHostname)}`);
+                // maj pid processes
+                process.request.pid = [...(process.request.pid ?? []), ...pids];
+            }
+            this.executionsRepository.save(process.request)
         }
+    }
+    
+    resetAllRunningProcess() {
+        this.orders.map(x => x.request).forEach(x => this.resetRunningProcess(x));
+    }
+    
+    /**
+     * Wait process orders or available ram change.
+     * @param ns 
+     * @returns new process orders
+     */
+    private async waitContextChange(): Promise<void> {
+        const requests: RamResourceExecution[] = this.executionOrdersService.getExecutionOrders();
+        const ramDisponible = this.serversService.getAllExecutable()
+                .map(x => this.ns.getServerMaxRam(x))
+                //.map(x => availableRam(ns, x))
+                .reduce((a,b) => a+b);
 
-        const executionPid: number = ns.exec(script.scriptsFilepath, executionOrder.sourceHostname, executionOrder.nbThread, ...script.args ?? []);
-        pids.push(executionPid);
+        const getId = (request: RamResourceExecution) => request.request.type + (request.request.target ?? '');
 
-        // TODO : reflechir à comment traiter le cas
-        if (executionPid === 0) {
+        let newRequest: RamResourceExecution[];
+        let newRamDisponible: number;
+        
+        do {
+            await this.ns.sleep(500);
+
+            newRamDisponible = this.serversService.getAllExecutable()
+                .map(x => this.ns.getServerMaxRam(x))
+                //.map(x => availableRam(ns, x))
+                .reduce((a,b) => a+b);
+
+            // remove from orders manual killed or script KO 
+            // TODO : alert ?
+            /*const killedOrders: RamResourceExecution[] = requests.filter(x => x.request.pid?.some(y => !ns.isRunning(y)));
+            if (killedOrders.length > 0) {
+                killedOrders.forEach(x => ExecutionsRepository.remove(ns, x.request));
+                ns.print('Execution order killed : ', killedOrders.map(x => x.request.type + ' ' + x.request.target));
+                break;
+            }*/
+            
+            newRequest = this.executionOrdersService.getExecutionOrders();
+        } while (
+            // requetes inchangées
+            Array.from(new Set([...requests.map(x => getId(x)), ...newRequest.map(x => getId(x))]))
+                .every(x => newRequest.map(x => getId(x)).includes(x) && requests.map(x => getId(x)).includes(x))
+            // RAM inchangée
+            && newRamDisponible === ramDisponible
+        )
+    }
+
+    /**
+     * Kill all process linked with process request, then clear pid on repository.
+     * @param ns 
+     * @param request process request to reset
+     */
+    private resetRunningProcess(request: ProcessRequest) {
+        // kill all process
+        request.pid?.filter(x => x !== undefined).forEach(x => this.ns.kill(x));
+        // reset all repository pid
+        request.pid = [];
+        this.executionsRepository.save(request)
+    }
+
+    //#region Execution
+    async execute(executionOrder: ExecutionOrder): Promise<number[]> {
+        const logger = new TerminalLogger(this.ns);
+        let pids: number[] = []
+        if (executionOrder.nbThread === 0) {
             return pids;
         }
-    }
 
-    return pids;
+        for (const script of executionOrder.request.scripts) {
+            // setup
+            if (!this.ns.fileExists(script.scriptsFilepath, executionOrder.sourceHostname)) {
+                logger.warn(`Script ${script.scriptsFilepath} inexistant sur ${executionOrder.sourceHostname}`);
+                const copyPid = this.ns.run(Referentiel.HACKING_DIRECTORY + '/spreading/copy-toolkit.worker.ts', 1, executionOrder.sourceHostname);
+                
+                await waitEndExecution(this.ns, copyPid);
+            }
+
+            const executionPid: number = this.ns.exec(script.scriptsFilepath, executionOrder.sourceHostname, executionOrder.nbThread, ...script.args ?? []);
+            pids.push(executionPid);
+
+            // TODO : reflechir à comment traiter le cas
+            if (executionPid === 0) {
+                return pids;
+            }
+        }
+
+        return pids;
+    }
+    //#endregion Execution
+
+    //#region Dashboard
+    setupDashboard() {
+        this.ns.disableLog("ALL");
+        this.ns.clearLog();
+        
+        Log.initTailTitle(this.ns, 'Executions', 'Scheduler');
+        this.ns.ui.openTail();
+    }
+    //#endregion Dashboard
 }
-//#endregion Execution
