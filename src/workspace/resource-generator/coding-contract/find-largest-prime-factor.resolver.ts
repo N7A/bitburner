09748c60de2934fefe06361eb899e065b6460b6a
Arthur <arthurnicolas2@hotmail.fr>
2025-09-21 14:53:59 +0200

    fix(contract): FindLargestPrimeFactor

--- src/workspace/resource-generator/coding-contract/find-largest-prime-factor.resolver.ts
+++ src/workspace/resource-generator/coding-contract/find-largest-prime-factor.resolver.ts
@@ -17,6 +17,13 @@ class FindLargestPrimeFactorResolver extends CodingContractResolver {
         );
     }
 
+    protected getSolution(codingContract: CodingContractObject): number {
+        const data: number = codingContract.data as number;
+        this.logger.trace(Log.INFO('Données', data));
+
+        return this.getSolutionI(data);
+    }
+
     /**
      * Description : A prime factor is a factor that is a prime number. 
      * 
@@ -25,45 +32,29 @@ class FindLargestPrimeFactorResolver extends CodingContractResolver {
      * @param codingContract 
      * @returns 
      */
-    protected getSolution(codingContract: CodingContractObject): number {
-        const data: number = codingContract.data as number;
-        this.logger.trace(Log.INFO('Données', data));
-
-        const primes: number[] = this.getPrimes(Math.floor(Math.sqrt(data)))
-            // DESC
-            .sort((a,b) => b - a);
-        for (const prime of primes) {
-            if (data % prime === 0) {
-                return prime;
-            }
+    private getSolutionI(data: number) {
+        if (data <= 1) {
+            return 1;
         }
 
-        return null;
-    }
-
-    /**
-     * Sieve of Eratosthenes algorithm.
-     * 
-     * @param maxNumber maximum number to check
-     * @returns 
-     */
-    private getPrimes(maxNumber: number): number[] {
-        // init result
-        let bulkPrime: boolean[] = new Array(maxNumber + 1).fill(true);
-
-        // par définition 0 et 1 ne sont pas premier
-        for (let i = 2; i * i <= maxNumber; i++) {
-            if (bulkPrime[i]) {
-                for (let j = i * i; j <= maxNumber; j += i) {
-                    bulkPrime[j] = false;
-                }
-            }
+        let factor = 2;
+        let secondFactor: number = data;
+        
+        // division tant que le résultat reste entier
+        while (secondFactor % factor === 0) {
+            secondFactor /= factor;
         }
 
-        return bulkPrime
-            .map((value: boolean, index: number) => [value, index] as [boolean, number])
-            .filter(x => x[0])
-            .map(x => x[1]);
+        do {
+            // facteur suivant
+            ++factor;
+            // division tant que le résultat reste entier
+            while (secondFactor % factor === 0) {
+                secondFactor /= factor;
+            }
+        } while (secondFactor > (factor) * (factor))
+
+        return (secondFactor === 1 ? factor : secondFactor);
     }
 
 }
