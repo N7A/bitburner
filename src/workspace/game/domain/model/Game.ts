4b05cf65a8cb0225a7d4bd6110c70a67631a6121
Arthur <arthurnicolas2@hotmail.fr>
2025-09-24 02:11:29 +0200

    feat(game) : add hasTIX et has4SDataTIXAPI

--- src/workspace/game/domain/model/Game.ts
+++ src/workspace/game/domain/model/Game.ts
@@ -2,5 +2,9 @@ export type Game = {
     //#region API
     hasFormulas: boolean;
     hasSingularity: boolean;
+    //#region Stock market
+    hasTIX: boolean;
+    has4SDataTIXAPI: boolean;
+    //#endregion Stock market
     //#endregion API
 } 
