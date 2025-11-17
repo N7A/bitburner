e3e43d8cda69b2dc55fb4581039bb83c0a876fec
PC-FIXE-N74-2023 <arthurnicolas2@hotmail.fr>
2025-11-17 21:37:20 +0100

    resource

---
+++ src/workspace/common/model/Resource.ts
@@ -0,0 +1,12 @@
+export enum ResourceType {
+    // gain de réputation pour une faction
+    FACTION_REPUTATION,
+    // gain de réputation pour une compagnie
+    COMPAGNY_REPUTATION,
+    // gain d'argent
+    MONEY,
+    // gain de RAM
+    RAM,
+    // TODO: up combat stats and charisma
+    DOWN_INFILTRATION_DIFFICULTY
+}
