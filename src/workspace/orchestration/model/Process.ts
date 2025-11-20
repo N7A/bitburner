5dcb9ef6d6e997055963b7a11e226e354716c76e
PC-FIXE-N74-2023 <arthurnicolas2@hotmail.fr>
2025-11-20 19:36:35 +0100

    feat(orchestrator)

--- src/workspace/orchestration/model/Process.ts
+++ src/workspace/orchestration/model/Process.ts
@@ -6,3 +6,17 @@ export type Process = {
     scriptUp: string;
     scriptDown: string;
 }
+
+export const PROCESSES: Process[] = [
+    {
+        need: ResourceType.RAM,
+        scriptUp: 'cmd/resource-generator/hacknet/ipvgo/play-board.daemon.ts',
+        scriptDown: null
+    },
+    {
+        need: ResourceType.RAM,
+        produce: ResourceType.FACTION_REPUTATION,
+        scriptUp: 'cmd/resource-generator/faction/get-backup-bonus.scheduler.ts',
+        scriptDown: null
+    }
+]
